484dc99730f248b0183a0d8e64c20c02b3c5f413
Natsuki <qq973313484@gmail.com>
2024-01-29 17:06:59 +0900

    環境変数の読み取りを変更した。

--- main.go
+++ main.go
@@ -5,14 +5,14 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
-	"github.com/gorilla/sessions"
+	"github.com/go-sql-driver/mysql"
+	"github.com/jmoiron/sqlx"
+	"github.com/joho/godotenv"
 	"github.com/labstack/echo-contrib/session"
 	"github.com/labstack/echo/v4"
-)
-
-var (
-	store = sessions.NewCookieStore([]byte(os.Getenv("SESSION_KEY")))
+	"github.com/srinathgs/mysqlstore"
 )
 
 func init() {
@@ -20,6 +20,46 @@ func init() {
 }
 
 func main() {
+	// .envファイルから環境変数を読み込み
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// データーベースの設定
+	jst, err := time.LoadLocation("Asia/Tokyo")
+	if err != nil {
+		log.Fatal(err)
+	}
+	conf := mysql.Config{
+		User:      os.Getenv("NS_MARIADB_USER"),
+		Passwd:    os.Getenv("NS_MARIADB_PASSWORD"),
+		Net:       "tcp",
+		Addr:      os.Getenv("NS_MARIADB_HOSTNAME") + ":" + os.Getenv("NS_MARIADB_PORT"),
+		DBName:    os.Getenv("NS_MARIADB_DATABASE"),
+		ParseTime: true,
+		Collation: "utf8mb4_unicode_ci",
+		Loc:       jst,
+	}
+
+	// データベースに接続
+	db, err := sqlx.Open("mysql", conf.FormatDSN())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// usersテーブルが存在しなかったら、usersテーブルを作成する
+	_, err = db.Exec("CREATE TABLE IF NOT EXISTS users (Username VARCHAR(255) PRIMARY KEY, HashedPass VARCHAR(255))")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// セッションの情報を記憶するための場所をデータベース上に設定
+	store, err := mysqlstore.NewMySQLStoreFromConnection(db.DB, "sessions", "/", 60*60*24*14, []byte("secret-token"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	// Echoの新しいインスタンスを作成
 	e := echo.New()
 	e.Use(session.Middleware(store))
@@ -30,7 +70,7 @@ func main() {
 	e.GET("/login", handler.HandleLogin)
 	e.GET("/callback", handler.HandleCallback)
 
-	err := e.Start(":8080")
+	err = e.Start(":8080")
 	if err != nil {
 		log.Fatal(err)
 	}
