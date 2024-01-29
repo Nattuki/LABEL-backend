0305b68ce9c76abe02f1bd2d1490c36e5dab8a40
Natsuki <qq973313484@gmail.com>
2024-01-29 17:35:43 +0900

    test内容を調整した

--- handler/OAuth.go
+++ handler/OAuth.go
@@ -119,7 +119,8 @@ func HandleCallback(c echo.Context) error {
 	sess.Values["access_token"] = token.AccessToken
 	sess.Save(c.Request(), c.Response())
 
-	return c.String(http.StatusCreated, token.AccessToken)
+	log.Println(sess.Values)
+	return c.String(http.StatusCreated, "Success!")
 }
 
 func RandomString(length int) (string, error) {
