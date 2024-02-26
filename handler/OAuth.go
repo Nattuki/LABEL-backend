443ea96c11c9e1bc0986394339d6fca092a54d7e
Natsuki <qq973313484@gmail.com>
2024-02-26 18:13:42 +0900

    loginPahtの返すパスを変更した

--- handler/OAuth.go
+++ handler/OAuth.go
@@ -72,7 +72,7 @@ func HandleGetOAuthUrl(c echo.Context) error {
 	q.Add("code_challenge", codeChallenge)
 	q.Add("code_challenge_method", "S256")
 	u.RawQuery = q.Encode()
-	return c.String(http.StatusOK, u.RawQuery)
+	return c.String(http.StatusOK, u.RequestURI())
 }
 
 func HandleCallback(c echo.Context) error {
