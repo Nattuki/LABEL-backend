ace1a0b2229f3e30bd0555bc4c7272023896df25
Natsuki <qq973313484@gmail.com>
2024-03-02 01:08:49 +0900

    logoutのエンドポイントを追加した

--- main.go
+++ main.go
@@ -66,7 +66,7 @@ func main() {
 	e.GET("/getme", handler.HandleGetMe)
 	e.GET("/loginpath", handler.HandleGetOAuthUrl)
 	e.GET("/gettoken", handler.HandleGetToken)
-
+	e.DELETE("/logout", handler.HandleLogout)
 	err = e.Start(":8080")
 	if err != nil {
 		log.Fatal(err)
