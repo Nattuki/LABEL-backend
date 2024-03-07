94a2a977cc6f3c72f4c86155572d8310cc84d4c4
Natsuki <qq973313484@gmail.com>
2024-03-07 17:42:21 +0900

    Message構造体にCreatorNameフィールドを追加した

--- handler/message.go
+++ handler/message.go
@@ -1,15 +1,18 @@
 package handler
 
 import (
+	"LABEL-backend/user"
 	"log"
 	"net/http"
 
+	"github.com/labstack/echo-contrib/session"
 	"github.com/labstack/echo/v4"
 )
 
 type Message struct {
-	Title string `json:"title"`
-	Url   string `json:"url"`
+	Title       string `json:"title"`
+	Url         string `json:"url"`
+	CreatorName string
 }
 
 func HandleMessage(c echo.Context) error {
@@ -19,6 +22,15 @@ func HandleMessage(c echo.Context) error {
 		log.Println(err)
 		return c.String(http.StatusInternalServerError, "failed to get the message")
 	}
+
+	sess, err := session.Get("LABEL_session", c)
+	if err != nil {
+		log.Println(err)
+		return c.String(http.StatusInternalServerError, "Failed to get the session.")
+	}
+	accessToken := sess.Values["access_token"]
+	message.CreatorName = user.GetName(accessToken.(string))
+
 	log.Println(*message)
 	return c.String(http.StatusOK, "OK!")
 }
